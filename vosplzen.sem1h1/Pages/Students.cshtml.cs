dd3aad0c9121a52af93bcc6f0808637eb24fc947
Petr Boháč <pbohac@windowslive.com>
2023-09-08 12:28:32 +0200

    split to dataprovider;

--- vosplzen.sem1h1/Pages/Students.cshtml.cs
+++ vosplzen.sem1h1/Pages/Students.cshtml.cs
@@ -1,75 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using vosplzen.sem1h1.Model;
+using vosplzen.sem1h1.Services;
 
 namespace vosplzen.sem1h1.Pages
 {
     public class StudentsModel : PageModel
     {
 
-        public List<Student> Students { get; set; }
+        public List<Student> Students { get; set; } = new List<Student>();
+
         public bool FilterIsOn { get; set; }
 
-        public void OnGet(string orderby = "lastname", string classfilterby)
+        public void OnGet(string orderby = "lastname", string classfilterby = "")
         {
-            Students = new List<Student>();
+            Students = DataProvider.GetStudents(orderby, classfilterby);
 
-            Students.Add(
-                new Student()
-                {
-                    Name = "Petr",
-                    Lastname = "Boháè",
-                    Email = "petr.bohac@codeclimber.cz",
-                    Class = "Master"
-                });
-
-            Students.Add(
-               new Student()
-               {
-                   Name = "Jaromír",
-                   Lastname = "Novák",
-                   Email = "jaromir.novak@escroom.cz",
-                   Class = "ESC01"
-               });
-
-            Students.Add(
-                  new Student()
-                  {
-                      Name = "Americo",
-                      Lastname = "Vespucci",
-                      Email = "americo.vespucci@volny.cz",
-                      Class = "SPAIN01"
-                  });
-
-            Students.Add(
-                 new Student()
-                 {
-                     Name = "Marylin",
-                     Lastname = "Monroe",
-                     Email = "marylin.monroe@tiscalli.cz",
-                     Class = "Master"
-                 });
-
-
-            if (orderby.Equals("lastname"))
-            {
-                Students = Students.OrderBy(x => x.Lastname).ToList();
-            }
-            else if (orderby.Equals("name"))
-            {
-                Students = Students.OrderBy(x => x.Name).ToList();
-            }
-            else if (orderby.Equals("email"))
-            {
-                Students = Students.OrderBy(x => x.Email).ToList();
-            }
-            else if (orderby.Equals("class"))
-            {
-                Students = Students.OrderBy(x => x.Class).ToList();
-            }
-
-
-            if (classfilterby is { })
+            if (classfilterby.Length > 0)
             {
                 Students = Students.Where(x => x.Class.Equals(classfilterby)).ToList();
                 FilterIsOn = true;
---
+++ vosplzen.sem1h1/Services/DataProvider.cs
@@ -0,0 +1,75 @@
+﻿using vosplzen.sem1h1.Model;
+
+namespace vosplzen.sem1h1.Services
+{
+    public class DataProvider
+    {
+
+
+
+        public static List<Student> GetStudents(string orderby = "lastname", string classfilterby = "")
+        {
+            List<Student> Students = new List<Student>();
+
+            Students.Add(
+                    new Student()
+                    {
+                        Name = "Petr",
+                        Lastname = "Boháč",
+                        Email = "petr.bohac@codeclimber.cz",
+                        Class = "Master"
+                    });
+
+            Students.Add(
+               new Student()
+               {
+                   Name = "Jaromír",
+                   Lastname = "Novák",
+                   Email = "jaromir.novak@escroom.cz",
+                   Class = "ESC01"
+               });
+
+            Students.Add(
+                  new Student()
+                  {
+                      Name = "Americo",
+                      Lastname = "Vespucci",
+                      Email = "americo.vespucci@volny.cz",
+                      Class = "SPAIN01"
+                  });
+
+            Students.Add(
+                 new Student()
+                 {
+                     Name = "Marylin",
+                     Lastname = "Monroe",
+                     Email = "marylin.monroe@tiscalli.cz",
+                     Class = "Master"
+                 });
+
+
+            if (orderby.Equals("lastname"))
+            {
+                Students = Students.OrderBy(x => x.Lastname).ToList();
+            }
+            else if (orderby.Equals("name"))
+            {
+                Students = Students.OrderBy(x => x.Name).ToList();
+            }
+            else if (orderby.Equals("email"))
+            {
+                Students = Students.OrderBy(x => x.Email).ToList();
+            }
+            else if (orderby.Equals("class"))
+            {
+                Students = Students.OrderBy(x => x.Class).ToList();
+            }
+
+
+
+            return Students;
+
+        }
+
+    }
+}
