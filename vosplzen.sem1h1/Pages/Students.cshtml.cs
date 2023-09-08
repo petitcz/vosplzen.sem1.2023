9a8790dd2763aa3abab92e7f531d924b81129cd8
Petr Boháč <pbohac@windowslive.com>
2023-09-08 11:16:25 +0200

    filterbyclass; orderby;

--- vosplzen.sem1h1/Pages/Students.cshtml.cs
+++ vosplzen.sem1h1/Pages/Students.cshtml.cs
@@ -8,8 +8,9 @@ namespace vosplzen.sem1h1.Pages
     {
 
         public List<Student> Students { get; set; }
+        public bool FilterIsOn { get; set; }
 
-        public void OnGet()
+        public void OnGet(string orderby = "lastname", string classfilterby = "")
         {
             Students = new List<Student>();
 
@@ -30,6 +31,53 @@ namespace vosplzen.sem1h1.Pages
                    Email = "jaromir.novak@escroom.cz",
                    Class = "ESC01"
                });
+
+            Students.Add(
+                  new Student()
+                  {
+                      Name = "Americo",
+                      Lastname = "Vespucci",
+                      Email = "americo.vespucci@volny.cz",
+                      Class = "Master"
+                  });
+
+            Students.Add(
+                 new Student()
+                 {
+                     Name = "Marylin",
+                     Lastname = "Monroe",
+                     Email = "marylin.monroe@tiscalli.cz",
+                     Class = "Master"
+                 });
+
+
+            if (orderby.Equals("lastname"))
+            {
+                Students = Students.OrderBy(x => x.Lastname).ToList();
+            }
+            else if (orderby.Equals("name"))
+            {
+                Students = Students.OrderBy(x => x.Name).ToList();
+            }
+            else if (orderby.Equals("email"))
+            {
+                Students = Students.OrderBy(x => x.Email).ToList();
+            }
+            else if (orderby.Equals("class"))
+            {
+                Students = Students.OrderBy(x => x.Class).ToList();
+            }
+
+
+            if(classfilterby.Length > 0)
+            {
+                Students = Students.Where(x => x.Class.Equals(classfilterby)).ToList();
+                FilterIsOn = true;
+            }
+            else
+            {
+                FilterIsOn = false;
+            }
 
         }
     }
