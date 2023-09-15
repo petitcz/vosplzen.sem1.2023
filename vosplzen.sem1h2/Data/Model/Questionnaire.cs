555b7e3a70e3fb79e41e04683f9c74dba220219f
Petr Boháč <pbohac@windowslive.com>
2023-09-15 09:41:46 +0200

    Initiatl - Questionnaire Basic;

---
+++ vosplzen.sem1h2/Data/Model/Questionnaire.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vosplzen.sem1h2.Data.Model
+{
+    public class Questionnaire
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+
+    }
+}
---
+++ vosplzen.sem1h2/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using vosplzen.sem1h2.Data.Model;
+
+namespace vosplzen.sem1h2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Questionnaire Questionnaire { get; set; } = new Questionnaire();
+
+        public string Message { get; set; } = String.Empty;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Message = $"Děkujeme za vyplnění dotazníku, Vaše hodnocení je: {Questionnaire.Rating}/5.";
+            Questionnaire = new Questionnaire();
+
+            ModelState.Clear();
+            
+            return Page();
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
