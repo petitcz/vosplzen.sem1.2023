7bf4b683b081f85fc03224a52be9c4f030781066
Petr Boháč <pbohac@windowslive.com>
2023-09-22 12:05:25 +0200

    allow null; abstract;

--- vosplzen.sem1h2/Generic/GenericPageModel.cs
+++ vosplzen.sem1h2/Generic/GenericPageModel.cs
@@ -3,14 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using vosplzen.sem1h2.Data.Model;
 using vosplzen.sem1h2.Generic;
-
-
+using System.Diagnostics.CodeAnalysis;
 
 namespace vosplzen.sem1h2.Generic
 {
-    public class GenericPageModel:PageModel
+    public abstract class GenericPageModel:PageModel
     {
-
+        [AllowNull]
         internal ApplicationDbContext _context;
 
         public string Message { get; set; } = String.Empty;
